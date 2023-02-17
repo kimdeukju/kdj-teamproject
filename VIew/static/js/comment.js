5d1b71542501ac66dd1e31df77ae81d663d5c4af
ckdtls1124 <ckdtls1124@naver.com>
2023-02-17 13:04:52 +0900

    0217_View_강창신

--- VIew/static/js/comment.js
+++ VIew/static/js/comment.js
@@ -1,14 +1,22 @@
 const commentColumn = document.getElementsByClassName('comment-column');
 const commentLists = document.querySelector('.comment-lists');
-const commentCon = document.getElementsByClassName('comment');
+const writeComment = document.querySelector('.commentInputCon');
+const commentInput =document.querySelector('#commentsInputData');
 const hide = 'hide';
 
 
 
 function showCommentsLists(e) {
+    console.log(e);
     commentLists.classList.toggle(hide);
 }
 
+function catchData(e){
+    e.preventDefault();
+    const commentInputData = commentInput.value;
+    localStorage.setItem('commentInputData', commentInputData);
+    console.log(localStorage.getItem('commentInputData'));
+}
 
-// commentColumn[0].addEventListener('click', showCommentsLists);
-commentCon[0].addEventListener('click', showCommentsLists);
+commentColumn[0].addEventListener('click', showCommentsLists);
+writeComment.addEventListener('submit', catchData);
