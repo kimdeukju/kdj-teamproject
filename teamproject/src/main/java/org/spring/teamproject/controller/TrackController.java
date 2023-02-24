bcbf07ddda34e6d95fb1da9ad7b3836e29c6974b
wkdgytjs <gytjs513563@gmail.com>
2023-02-24 17:56:36 +0900

    0224 장효선

--- teamproject/src/main/java/org/spring/teamproject/controller/TrackController.java
+++ teamproject/src/main/java/org/spring/teamproject/controller/TrackController.java
@@ -8,37 +8,44 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping("/admin")
 @RequiredArgsConstructor
-public class AdminController {
+public class TrackController {
 
     private final ItemService itemService;
 
-    @GetMapping("/trackAdd")
-    public String addView(Model model){
-        model.addAttribute("itemDto",new ItemDto());
+    @GetMapping("/admin/trackAdd")
+    public String addView(Model model) {
+        model.addAttribute("itemDto", new ItemDto());
 
-        return "/pages/pages/trackInsert";
+        return "/pages/admin/trackInsert";
     }
 
     @PostMapping("/trackAdd")
     public String addmet(@Valid ItemDto itemDto, BindingResult bindingResult) throws IOException {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "/pages/admin/trackInsert";
         }
-
         itemService.itemInsert(itemDto);
 
         return "redirect:/trackAdd";
-
     }
 
+    @GetMapping("/tracks")
+    public String itemList(Model model) {
 
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        model.addAttribute("itemDtoList", itemDtoList);
+
+        return "itemList";
+    }
 }
+
